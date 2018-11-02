74c3db93e7fc4f802e399aaa3bd580a8865bb4d1
zhangyonglele <zhangyonglele@yeah.net>
2018-11-02 20:41:58 +0800

    final test 2

--- javalear/web4/src/Test.java
+++ javalear/web4/src/Test.java
@@ -1,4 +1,11 @@
-package PACKAGE_NAME;
+import JDBC.JDBC;
+
+import java.sql.ResultSet;
 
 public class Test {
+    public static void main(String[] args){
+        JDBC.init();
+        ResultSet rs = JDBC.selectData("SELECT * FROM manager WHERE id='1'");
+        System.out.println(rs);
+    }
 }
